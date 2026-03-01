06542dad4f0792e0fa915ee4c711b5b8f49594f6
Cohen Carlisle <cohencarlisle+github@pm.me>
2026-02-28 23:22:35 -0500

    mix new inv_map

---
+++ lib/inv_map.ex
@@ -0,0 +1,18 @@
+defmodule InvMap do
+  @moduledoc """
+  Documentation for `InvMap`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> InvMap.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/inv_map_test.exs
@@ -0,0 +1,8 @@
+defmodule InvMapTest do
+  use ExUnit.Case
+  doctest InvMap
+
+  test "greets the world" do
+    assert InvMap.hello() == :world
+  end
+end
