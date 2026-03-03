f31ed7e00781bbf96db439b17b003e0c801ec689
Cohen Carlisle <cohencarlisle+github@pm.me>
2026-03-02 19:10:07 -0500

    put/3 and get/3

--- lib/inv_map.ex
+++ lib/inv_map.ex
@@ -17,4 +17,39 @@ defmodule InvMap do
   def new do
     %InvMap{}
   end
+
+  @doc """
+  Puts the given `value` under `key` in `inv_map`.
+
+  ## Examples
+
+      iex> InvMap.put(InvMap.new(), :a, 1)
+      %InvMap{forward: %{:a => 1}, inverse: %{1 => :a}}
+
+  """
+  def put(%InvMap{forward: forward, inverse: inverse} = inv_map, key, value) do
+    # TODO: check for existing keys
+    %{inv_map | forward: Map.put(forward, key, value), inverse: Map.put(inverse, value, key)}
+  end
+
+  @doc """
+  Gets the valu for the specific `key` in `inv_map`.
+
+  if `key` is present in `inv_map` then its value is returned.
+  Otherwise, `default` is returned.
+
+  If `default` is not provided, `nil` is used.
+
+  ## Examples
+
+      iex> InvMap.get(InvMap.new(), :a)
+      nil
+      iex> InvMap.get(InvMap.new(), :a, 0)
+      0
+
+  """
+  def get(%InvMap{forward: forward, inverse: inverse}, key, default \\ nil) do
+    # TODO: rewrite like https://github.com/elixir-lang/elixir/blob/v1.19.5/lib/elixir/lib/map.ex#L532 ?
+    Map.get_lazy(forward, key, fn -> Map.get(inverse, key, default) end)
+  end
 end
--- test/inv_map_test.exs
+++ test/inv_map_test.exs
@@ -7,4 +7,21 @@ defmodule InvMapTest do
       assert InvMap.new() == %InvMap{forward: %{}, inverse: %{}}
     end
   end
+
+  describe "put/3" do
+    test "puts the key value pair in inv_map" do
+      assert InvMap.put(InvMap.new(), :a, 1) == %InvMap{forward: %{:a => 1}, inverse: %{1 => :a}}
+    end
+  end
+
+  describe "get/3" do
+    test "returns the value of key in inv_map, else default" do
+      inv_map = InvMap.new()
+      inv_map = InvMap.put(inv_map, :a, 1)
+      assert InvMap.get(inv_map, :a) == 1
+      assert InvMap.get(inv_map, 1) == :a
+      assert InvMap.get(inv_map, :no) == nil
+      assert InvMap.get(inv_map, :no, :yes) == :yes
+    end
+  end
 end
